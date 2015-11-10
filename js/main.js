fd0d6f92da669f9b78f8f0b53e6267e0fb418a8d
Seung Lee <shl7@uw.edu>
2015-11-09 23:58:44 -0800

    your playlist added

--- js/main.js
+++ js/main.js
@@ -1,34 +1,33 @@
 var data;
-
-var baseUrl = 'https://api.spotify.com/v1/search?type=track&query=';
+var user;
 
 var spotifyApp = angular.module("spotifyApp", ['spotify']);
 
 spotifyApp.config(function (SpotifyProvider) {
   SpotifyProvider.setClientId('3c3ac8bf947448cda2e3a000f9f756b7');
-  SpotifyProvider.setRedirectUri('http://localhost:8080/callback');
+  SpotifyProvider.setRedirectUri('http://localhost:8080/callback.html');
   SpotifyProvider.setScope('user-read-private playlist-read-private user-library-read');
-  // // If you already have an auth token
-  // SpotifyProvider.setAuthToken('BQCa1C32O8PG6yB-bvBEG2VrhVqEL1KNhU3k0Zd1y3qIIPU2pBMGqh5jDS8kQ4ICaF6BvcNTDMUsRqSiXx7C5sIuFLNw3kdHfd4aemYGU6w084RXFRyT62mDS1bTgPL370mjZ9TCJPYzzp6EDNfC5ertmKTbM5UVSMN36ZYtvMakqWowU9c');
+  // If you already have an auth token
+  SpotifyProvider.setAuthToken(localStorage.getItem("spotify-token"));
 });
 
-spotifyApp.controller('spotifyCtrl', function(Spotify, $scope, $http) {
+spotifyApp.controller('spotifyCtrl', function (Spotify, $scope, $http) {
+
+	// check if someone is logged in
+	Spotify.getCurrentUser().then(function (data) {
+	  console.log(data);
+	  user = data;
+	  $("header").append($("<p>").text("Welcome, " + data.id + "!"));
+	}, function () {
+		console.log("not logged in");
+		$("#inbtn").css("display", "inline");
+	});
 
     $scope.audioObject = {}
 
-    // $scope.getSongs = function() {
-    // 	$http.get(baseUrl + $scope.searchTrack).success(function(response){
-    // 		data = $scope.tracks = response.tracks.items;
-    //     	$scope.track = data[0];
-
-    // 	})
-    // }
-
-    var accessToken = "BQBM39LFWLfeXuxoByc8g4LXqz2_BMQnWHfbyF6MYUNrYM_fu1wo7W7avTTUrP8saZddY8DbRLIARfYEhlm9Kv4KlG56sZqvefQl_Qj2p9vCG61MCGpiYnUAlMJiunY2GA40aSCaeRDjapqWXeEPQfwhPfMs60zZ8Y-sia9OFMakXoETp0Y";
+    var accessToken = localStorage.getItem("spotify-token");
 
     var featured = "https://api.spotify.com/v1/browse/featured-playlists";
-    var playlistID = "";
-    var ownerID = "";
     var songName = "";
     var artist = "";
     var album = "";
@@ -40,11 +39,12 @@ spotifyApp.controller('spotifyCtrl', function(Spotify, $scope, $http) {
 			headers: {
 				'Authorization': 'Bearer ' + accessToken
 		}})
-		.success(function(response) {
-	    		$scope.playlistName = "(From the playlist " + response.playlists.items[0].name + ")";
-	    		getTracks(response.playlists.items[0].id, response.playlists.items[0].owner.id);
+		.success(function (response) {
+			var random = randomNumber(response.playlists.items.length);
+			console.log("first random: " + random);
+    		$scope.playlistName = "(From the playlist " + response.playlists.items[random].name + ")";
+    		getTracks(response.playlists.items[random].id, response.playlists.items[random].owner.id);
 		});
-		
    	}
 
    	var getTracks = function(playlistID, ownerID) {
@@ -54,12 +54,25 @@ spotifyApp.controller('spotifyCtrl', function(Spotify, $scope, $http) {
 			url: trackURL,
 			headers: {
 				'Authorization': 'Bearer '+ accessToken
-		}}).success(function(response) {
-			console.log(response);
-			$scope.track = response.items[0].track;
-			songName = respnose.items[0].track.name;
-			artist = response.items[0].artists.name;
-			album = response.items[0].album.name;
+		}}).success(function (response) {
+			var random = randomNumber(response.items.length);
+			console.log("second random: " + random);
+			console.log(response.items[random]);
+			$scope.track = response.items[random].track;
+			songName = $scope.song = response.items[random].track.name;
+			artist = $scope.artist = response.items[random].track.artists[0].name;
+			album = $scope.album = response.items[random].track.album.name;
+		});
+   	}
+	
+	// if logged in
+   	$scope.userPlaylist = function() {
+	
+		Spotify.getUserPlaylists(user.id).then(function (data) {
+			var random = randomNumber(data.items.length);
+			console.log("first random: " + random);
+    		$scope.playlistName = "(From the playlist " + data.items[random].name + ")";
+    		getTracks(data.items[random].id, data.items[random].owner.id);
 		});
    	}
 
@@ -81,40 +94,26 @@ spotifyApp.controller('spotifyCtrl', function(Spotify, $scope, $http) {
 	$scope.login = function() {
 		Spotify.login().then(function(data){
 			console.log(data);
-			alert("you are now logged in");
+			console.log("you are now logged in");
 		}, function() {
 			console.log(data);
 			console.log("didn\'t log in");
 		});
-		$("#username").val("");
-		$("#password").val("");
 		$("#inbtn").css("display", "none");
-		$("#outbtn").css("display", "inline");
 	};
-
-	$scope.logOut = function() {
-		Spotify.login();
-		$("#inbtn").css("display", "inline");
-		$("#outbtn").css("display", "none");
-	}
-
-
 
 	// Spotify.getNewReleases().then(function (data) {
 	// 	console.log(data);
 	// })
 
-	// // if logged in
-	// Spotify.getUserPlaylists('user_id').then(function (data) {
-	// 	console.log(data);
-	// });
+
 
 	// Spotify.getPlaylistTracks('user_id', 'playlist_id').then(function (data) {
 	// 	console.log(data);
 	// })
 
-	// Spotify.getCurrentUser().then(function (data) {
-	//   console.log(data);
-	// });
-})
+});
 
+var randomNumber = function(max) {
+	return Math.floor((Math.random() * parseInt(max)));
+}
